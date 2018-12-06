196bd9ac0a5d26b28e3f10af685c400ef765c4ac
gerson <gersonrevatta@gmail.com>
2018-12-06 18:48:20 -0500

    Create comment to Note

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,19 @@
 # CommentsController
 class CommentsController < ApplicationController
+  before_action :find_note
+
+  def create
+    @comment = @note.comments.new(comment_params)
+    return redirect_to @note if @comment.save
+  end
+
+  private
+
+  def find_note
+    @note = Note.find(params[:note_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :note_id)
+  end
 end
