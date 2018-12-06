f78586b20c6600c5db1200ac3d9ba90750f076c3
gerson <gersonrevatta@gmail.com>
2018-12-06 16:13:59 -0500

    Add create to Note CRUD

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,3 +1,25 @@
 # CategoriesController
 class NotesController < ApplicationController
+  def index
+    @notes = Note.all.order('created_at DESC')
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :description)
+  end
 end
